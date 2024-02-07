3f8885cff4a5613782ef3dbf05a4df2a9de22cd9
0x4net <121145040+0x4net@users.noreply.github.com>
2024-02-07 09:59:35 -0600

    Update scan_bacnet_subnet_dense.py

--- scan_bacnet_subnet_dense.py
+++ scan_bacnet_subnet_dense.py
@@ -1,11 +1,10 @@
 import BAC0
 import time
 
-myIP = '192.168.1.4/24'
+myIP = '<Your IP>/<MASK>' #You need to be on the same subnet as the bacnet device. Example: '192.168.1.4/24'
 bacnet = BAC0.connect(ip=myIP)
-bacnet.whois()
-print("Sleeping...")
-time.sleep(5)
+bacnet.whois() #Broadcast request of bacnet devices
+time.sleep(5)  #Wait for devices to respond
 for i, (deviceId, companyId, devIp, numDeviceId) in enumerate(bacnet.devices):
     print(f"-------- Device #{numDeviceId} --------")
     print(f"Device:     {deviceId}")
@@ -14,4 +13,4 @@ for i, (deviceId, companyId, devIp, numDeviceId) in enumerate(bacnet.devices):
     readDevice = bacnet.readMultiple(f"{devIp} device {numDeviceId} all")
     print(f"Model Name: {readDevice[11]}")
     print(f"Version:    {readDevice[2]}")
-    # print(readDevice)
+    # print(readDevice) #List all available info about the device
